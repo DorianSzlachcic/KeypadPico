b9920be8747f523fc837e63448640944e297e038
DorianSzlachcic <dorianszlachcic@gmail.com>
2023-03-13 16:58:55 +0100

    Some changes

--- main.c
+++ main.c
@@ -1,35 +1,38 @@
 #include <stdlib.h>
 
-#include "lib/GUI/GUI_Paint.h"
-#include "lib/Fonts/fonts.h"
-#include "lib/e-Paper/EPD_2in9_V2.h"
+#include "pico/stdlib.h"
 
-void Loading_Screen(UBYTE* Image)
-{
-    Paint_NewImage(Image, EPD_2IN9_V2_WIDTH, EPD_2IN9_V2_HEIGHT, 90, WHITE);
-    Paint_Clear(WHITE);
+#include "GUI_Paint.h"
+#include "EPD_2in9_V2.h"
 
-    Paint_DrawString_EN(50, 50, "Loading", &Font12, WHITE, BLACK);
+#include "hardware/flash.h"
+#include "hardware/sync.h"
 
-    EPD_2IN9_V2_Display(Image);
-
-    DEV_Delay_ms(1000);
-
-    Paint_NewImage(Image, EPD_2IN9_V2_WIDTH, EPD_2IN9_V2_HEIGHT, 90, WHITE);
-    Paint_Clear(WHITE);
-
-    Paint_DrawString_EN(50, 50, "Loading", &Font12, WHITE, BLACK);
-    Paint_DrawPoint(100, 50, BLACK, DOT_PIXEL_4X4, DOT_FILL_RIGHTUP);
-
-    EPD_2IN9_V2_Display(Image);
-
-    DEV_Delay_ms(1000);
-
-    Paint_NewImage(Image, EPD_2IN9_V2_WIDTH, EPD_2IN9_V2_HEIGHT, 90, WHITE);
-    Paint_Clear(WHITE);
-    EPD_2IN9_V2_Display(Image);
-
+void readfile(unsigned char* buffer, const char* filename) {
+    FILE *f;
+    if (f = fopen(filename, "rb"))
+    {
+        buffer = (unsigned char*)malloc(64*64);
+        fseek(f, 54, SEEK_SET);
+        fread(buffer, 64*64*4, 1, f);
+        fclose(f);
+    }
+    else {
+        const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+    }
+    }
+    
+    
+    
 }
+
 
 int main(void)
 {
@@ -46,6 +49,18 @@ int main(void)
         printf("Failed memory allocation...\r\n");
         return -1;
     }
+
+    unsigned char* fileImage;
+    readfile(fileImage, "/icon.bmp");
+
+    Paint_NewImage(Image, EPD_2IN9_V2_WIDTH, EPD_2IN9_V2_HEIGHT, 90, WHITE);
+    Paint_Clear(WHITE);
+
+    Paint_DrawBitMap(fileImage);
+
+    EPD_2IN9_V2_Display(Image);
+
+    DEV_Delay_ms(3000);
 
     EPD_2IN9_V2_Clear();
 
