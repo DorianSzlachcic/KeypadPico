b1b12b41da166b58a2808be234738508c465f3cf
DorianSzlachcic <dorianszlachcic@gmail.com>
2023-03-16 08:26:03 +0100

    Fight with virtual filesystem, will focus on displaying icons from const vars now

--- main.c
+++ main.c
@@ -1,24 +1,17 @@
 #include <stdlib.h>
+#include <lfs.h>
 
 #include "pico/stdlib.h"
 
 #include "GUI_Paint.h"
 #include "EPD_2in9_V2.h"
+#include "filesystem.h"
 
-#include "hardware/flash.h"
-#include "hardware/sync.h"
+#define BUFFER_LENGTH 256
 
-void readfile(unsigned char* buffer, const char* filename) {
-    FILE *f;
-    if (f = fopen(filename, "rb"))
-    {
-        buffer = (unsigned char*)malloc(64*64);
-        fseek(f, 54, SEEK_SET);
-        fread(buffer, 64*64*4, 1, f);
-        fclose(f);
-    }
-    else {
-        const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+void blink()
+{
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
@@ -26,16 +19,38 @@ void readfile(unsigned char* buffer, const char* filename) {
         sleep_ms(250);
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
+        break;
     }
+}
+
+uint16_t get_block(uint8_t *buffer) {
+  uint16_t buffer_index= 0;
+  while (true) {
+    int c = getchar_timeout_us(100);
+    if (c != PICO_ERROR_TIMEOUT && buffer_index < BUFFER_LENGTH) {
+      buffer[buffer_index++] = (c & 0xFF);
+    } else {
+      break;
     }
-    
-    
-    
+  }
+  return buffer_index;
 }
 
 
 int main(void)
 {
+    int buff_index = 0;
+
+    unsigned char* fileImage = (unsigned char*)malloc(BUFFER_LENGTH);
+
+    while (buff_index < 1)
+    {
+        buff_index = get_block(fileImage);
+        printf("wait\n");
+    }
+
+    blink();
+
 
     if(DEV_Module_Init()!=0){
         return -1;
@@ -43,19 +58,15 @@ int main(void)
     EPD_2IN9_V2_Init();
     EPD_2IN9_V2_Clear();
 
-	UBYTE* Image;
+    UBYTE* Image;
     UWORD Imagesize = ((EPD_2IN9_V2_WIDTH % 8 == 0)? (EPD_2IN9_V2_WIDTH / 8 ): (EPD_2IN9_V2_WIDTH / 8 + 1)) * EPD_2IN9_V2_HEIGHT;
     if((Image = (UBYTE *)malloc(Imagesize)) == NULL) {
         printf("Failed memory allocation...\r\n");
         return -1;
     }
 
-    unsigned char* fileImage;
-    readfile(fileImage, "/icon.bmp");
-
     Paint_NewImage(Image, EPD_2IN9_V2_WIDTH, EPD_2IN9_V2_HEIGHT, 90, WHITE);
     Paint_Clear(WHITE);
-
     Paint_DrawBitMap(fileImage);
 
     EPD_2IN9_V2_Display(Image);
---
+++ setup.sh
@@ -0,0 +1,1 @@
+export PICO_SDK_PATH=../thirdparty/pico-sdk
