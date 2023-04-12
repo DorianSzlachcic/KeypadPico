dad185dfe5164661a76df9c5cfdff8a84180387b
DorianSzlachcic <dorianszlachcic@gmail.com>
2023-04-12 09:48:59 +0200

    Using pico as hid and cdc device, playing with filesystem(commented)

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 
 #include "pico/stdlib.h"
 
+#include "lfs.h"
+
 #include "bsp/board.h"
 #include "tusb.h"
 
@@ -11,18 +13,18 @@
 
 #include "display.h"
 #include "button.h"
+#include "filesystem.h"
 
-void blink()
+void blink(int t)
 {
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
+    for(int i = 0; i<t;i++) {
         gpio_put(LED_PIN, 1);
         sleep_ms(250);
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
-        break;
     }
 }
 
@@ -41,13 +43,45 @@ int main(void)
 
     buttons_init();
     board_init();
-    tud_init(0);
+    tusb_init();
+
     while(true)
     {
         tud_task();
         unsigned char buttons_state = read_buttons();
         buttons_task(buttons_state);
+        printf("hello");
     }
+    // lfs_t lfs;
+    // lfs_file_t file;
+
+    // // mount the filesystem
+    // int err = lfs_mount(&lfs, &PICO_FLASH_CFG);
+
+    // // reformat if we can't mount the filesystem
+    // // this should only happen on the first boot
+    // if (err) {
+    //     lfs_format(&lfs, &PICO_FLASH_CFG);
+    //     lfs_mount(&lfs, &PICO_FLASH_CFG);
+    // }
+
+    // // read current count
+    // uint32_t boot_count = 0;
+    // lfs_file_open(&lfs, &file, "boot_count", LFS_O_RDWR | LFS_O_CREAT);
+    // lfs_file_read(&lfs, &file, &boot_count, sizeof(boot_count));
+
+    // // update boot count
+    // boot_count += 1;
+    // lfs_file_rewind(&lfs, &file);
+    // lfs_file_write(&lfs, &file, &boot_count, sizeof(boot_count));
+
+    // // remember the storage is not updated until the file is closed successfully
+    // lfs_file_close(&lfs, &file);
+
+    // // release any resources we were using
+    // lfs_unmount(&lfs);
+
+    // blink(boot_count);
     
     return 0;
 }
